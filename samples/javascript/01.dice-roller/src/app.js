6da7e87c402a02e945cd9eeaed83535057250f2f
Sathya Pandalai <125775867+spandalai26@users.noreply.github.com>
2023-05-31 15:24:59 -0400

    Changes for testing

--- samples/javascript/01.dice-roller/src/app.js
+++ samples/javascript/01.dice-roller/src/app.js
@@ -3,82 +3,158 @@
  * Licensed under the MIT License.
  */
 
-import {
-    LivePresence,
-    LiveShareClient,
-    LiveState,
-    TestLiveShareHost,
-} from "@microsoft/live-share";
-import { app, LiveShareHost } from "@microsoft/teams-js";
-import { renderMeetingStage } from "./renderMeetingStage";
-import { renderMeetingSidePanel } from "./renderMeetingSidePanel";
-import { renderTabConfig } from "./renderTabConfig";
-import { renderError } from "./renderError";
 import { SharedMap } from "fluid-framework";
+import { app, pages, LiveShareHost } from "@microsoft/teams-js";
+import { LiveShareClient, TestLiveShareHost } from "@microsoft/live-share";
+import { InsecureTokenProvider } from "@fluidframework/test-client-utils";
 
-const searchParams = new URL(window.location.href).searchParams;
+const searchParams = new URL(window.location).searchParams;
 const root = document.getElementById("content");
-let theme = "light";
 
 // Define container schema
+
+const diceValueKey = "dice-value-key";
+
 const containerSchema = {
-    initialObjects: {
-        diceState: LiveState, // shared dice value, resets once all users close app
-        presence: LivePresence, // each user has their own dice value, resets once all users close app
-        storedDiceMap: SharedMap, // stored dice value that will last 6 hours from session creation
-    },
+  initialObjects: { diceMap: SharedMap },
 };
+
+function onContainerFirstCreated(container) {
+  // Set initial state of the rolled dice to 1.
+  container.initialObjects.diceMap.set(diceValueKey, 1);
+}
 
 // STARTUP LOGIC
 
 async function start() {
-    // Check for page to display
-    let view = searchParams.get("view") || "stage";
+  // Check for page to display
+  let view = searchParams.get("view") || "stage";
 
-    // Check if we are running on stage.
-    if (searchParams.get("inTeams")) {
-        // Initialize teams app
-        await app.initialize();
-        // Get Teams app context to get the initial theme
-        const context = await app.getContext();
-        theme = context.app.theme === "default" ? "light" : "dark";
-        app.registerOnThemeChangeHandler((theme) => {
-            theme = theme === "default" ? "light" : "dark";
-        });
-    }
+  // Check if we are running on stage.
+  if (!!searchParams.get("inTeams")) {
+    // Initialize teams app
+    await app.initialize();
 
-    // Load the requested view
-    switch (view) {
-        case "content": {
-            const { container } = await joinContainer();
-            await renderMeetingSidePanel(container, root, theme);
-            break;
-        }
-        case "config": {
-            renderTabConfig(root, theme);
-            break;
-        }
-        case "stage":
-        default: {
-            const { container } = await joinContainer();
-            await renderMeetingStage(container, root, theme);
-            break;
-        }
+    // Get our frameContext from context of our app in Teams
+    const context = await app.getContext();
+    if (context.page.frameContext == "meetingStage") {
+      view = "stage";
     }
+  }
+
+  // Load the requested view
+  switch (view) {
+    case "content":
+      renderSidePanel(root);
+      break;
+    case "config":
+      renderSettings(root);
+      break;
+    case "stage":
+    default:
+      const { container } = await joinContainer();
+      renderStage(container.initialObjects.diceMap, root);
+      break;
+  }
 }
+
+start().catch((error) => console.error(error));
 
 async function joinContainer() {
-    // Are we running in teams?
-    const host =
-        searchParams.get("inTeams") === "true"
-            ? LiveShareHost.create()
-            : TestLiveShareHost.create();
-
+    // Are we running in teams? If so, use LiveShareHost, otherwise use TestLiveShareHost
+    const host = !!searchParams.get("inTeams")
+      ? LiveShareHost.create()
+      : TestLiveShareHost.create();
     // Create client
-    const client = new LiveShareClient(host);
-
+    const liveShare = new LiveShareClient(host);
     // Join container
-    return await client.joinContainer(containerSchema);
+    return await liveShare.joinContainer(containerSchema, onContainerFirstCreated);
+  }
+
+
+const stageTemplate = document.createElement("template");
+
+stageTemplate["innerHTML"] = `
+  <div class="wrapper">
+    <div class="dice"></div>
+    <button class="roll"> Roll </button>
+  </div>
+`;
+function renderStage(diceMap, elem) {
+  elem.appendChild(stageTemplate.content.cloneNode(true));
+  const rollButton = elem.querySelector(".roll");
+  const dice = elem.querySelector(".dice");
+
+  rollButton.onclick = () => updateDice(Math.floor(Math.random() * 6) + 1);
+
+  const updateDice = (value) => {
+    // Unicode 0x2680-0x2685 are the sides of a die (⚀⚁⚂⚃⚄⚅).
+    dice.textContent = String.fromCodePoint(0x267f + value);
+  };
+  updateDice(1);
 }
 
-start().catch((error) => renderError(root, error));
+
+rollButton.onclick = () =>
+  diceMap.set("dice-value-key", Math.floor(Math.random() * 6) + 1);
+
+
+const updateDice = () => {
+  const diceValue = diceMap.get("dice-value-key");
+  dice.textContent = String.fromCodePoint(0x267f + diceValue);
+};
+updateDice();
+
+diceMap.on("valueChanged", updateDice);
+
+
+const sidePanelTemplate = document.createElement("template");
+
+
+sidePanelTemplate["innerHTML"] = `
+  <style>
+    .wrapper { text-align: center }
+    .title { font-size: large; font-weight: bolder; }
+    .text { font-size: medium; }
+  </style>
+  <div class="wrapper">
+    <p class="title">Lets get started</p>
+    <p class="text">Press the share to stage button to share Dice Roller to the meeting stage.</p>
+  </div>
+`;
+
+function renderSidePanel(elem) {
+  elem.appendChild(sidePanelTemplate.content.cloneNode(true));
+}
+
+const settingsTemplate = document.createElement("template");
+
+settingsTemplate["innerHTML"] = `
+  <style>
+    .wrapper { text-align: center }
+    .title { font-size: large; font-weight: bolder; }
+    .text { font-size: medium; }
+  </style>
+  <div class="wrapper">
+    <p class="title">Welcome to Dice Roller!</p>
+    <p class="text">Press the save button to continue.</p>
+  </div>
+`;
+
+function renderSettings(elem) {
+  elem.appendChild(settingsTemplate.content.cloneNode(true));
+
+  // Save the configurable tab
+  pages.config.registerOnSaveHandler((saveEvent) => {
+    pages.config.setConfig({
+      websiteUrl: window.location.origin,
+      contentUrl: window.location.origin + "?inTeams=1&view=content",
+      entityId: "DiceRollerFluidLiveShare",
+      suggestedDisplayName: "DiceRollerFluidLiveShare",
+    });
+    saveEvent.notifySuccess();
+  });
+
+  // Enable the Save button in config dialog
+  pages.config.setValidityState(true);
+}
---
+++ samples/javascript/01.dice-roller/src/old_app.js
@@ -0,0 +1,85 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+    LivePresence,
+    LiveShareClient,
+    LiveState,
+    TestLiveShareHost,
+} from "@microsoft/live-share";
+import { app, pages, LiveShareHost } from "@microsoft/teams-js";
+import { renderMeetingStage } from "./renderMeetingStage";
+import { renderMeetingSidePanel } from "./renderMeetingSidePanel";
+import { renderTabConfig } from "./renderTabConfig";
+import { renderError } from "./renderError";
+import { SharedMap } from "fluid-framework";
+import { InsecureTokenProvider } from "@fluidframework/test-client-utils";
+
+const searchParams = new URL(window.location.href).searchParams;
+const root = document.getElementById("content");
+let theme = "light";
+
+// Define container schema
+const containerSchema = {
+    initialObjects: {
+        diceState: LiveState, // shared dice value, resets once all users close app
+        presence: LivePresence, // each user has their own dice value, resets once all users close app
+        storedDiceMap: SharedMap, // stored dice value that will last 6 hours from session creation
+    },
+};
+
+// STARTUP LOGIC
+
+async function start() {
+    // Check for page to display
+    let view = searchParams.get("view") || "stage";
+
+    // Check if we are running on stage.
+    if (searchParams.get("inTeams")) {
+        // Initialize teams app
+        await app.initialize();
+        // Get Teams app context to get the initial theme
+        const context = await app.getContext();
+        theme = context.app.theme === "default" ? "light" : "dark";
+        app.registerOnThemeChangeHandler((theme) => {
+            theme = theme === "default" ? "light" : "dark";
+        });
+    }
+
+    // Load the requested view
+    switch (view) {
+        case "content": {
+            const { container } = await joinContainer();
+            await renderMeetingSidePanel(container, root, theme);
+            break;
+        }
+        case "config": {
+            renderTabConfig(root, theme);
+            break;
+        }
+        case "stage":
+        default: {
+            const { container } = await joinContainer();
+            await renderMeetingStage(container, root, theme);
+            break;
+        }
+    }
+}
+
+async function joinContainer() {
+    // Are we running in teams?
+    const host =
+        searchParams.get("inTeams") === "true"
+            ? LiveShareHost.create()
+            : TestLiveShareHost.create();
+
+    // Create client
+    const client = new LiveShareClient(host);
+
+    // Join container
+    return await client.joinContainer(containerSchema);
+}
+
+start().catch((error) => renderError(root, error));
